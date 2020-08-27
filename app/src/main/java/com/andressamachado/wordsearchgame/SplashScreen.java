9acb5db5e3733f320626141fe466aa306b3953b5
AndressaMachado <andressa.pamachado@gmail.com>
2020-08-26 20:27:50 -0400

    Adds splash screen with simple animation

---
+++ app/src/main/java/com/andressamachado/wordsearchgame/SplashScreen.java
@@ -0,0 +1,53 @@
+package com.andressamachado.wordsearchgame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    private static final int SPLASH_SCREEN = 4000;
+
+    Animation topAnimation, bottomAnimation;
+    TextView title, subtitle;
+    ImageView image;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //set the splash screen to be full screen, not using the action bar that is placed by default by android
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+
+        //Initializing animations
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        //Initializing every view used in this activity
+        image = findViewById(R.id.splash_scree_img);
+        title = findViewById(R.id.splash_scree_title);
+        subtitle = findViewById(R.id.splash_scree_subtitle);
+
+        //Setting animation
+        image.setAnimation(bottomAnimation);
+        title.setAnimation(topAnimation);
+        subtitle.setAnimation(topAnimation);
+
+        //4seconds past, run next application
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent goToApplication = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(goToApplication);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+    }
+}
